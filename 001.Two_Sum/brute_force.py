5ce951f71e81d2c942a14092deab05af0ea60062
Victor <victor@debian.local>
2018-03-12 23:54:53 -0300

    new file:   001.Two_Sum/brute_force.py 	new file:   001.Two_Sum/dictionary.py

---
+++ 001.Two_Sum/brute_force.py
@@ -0,0 +1,11 @@
+# Brute force approach
+
+class Solution:
+
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
+
+Solution().twoSum([2, 7, 8], 15)
---
+++ 001.Two_Sum/dictionary.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums, target):
+        lookup = {}
+        for index, number in enumerate(nums):
+            if target - number in lookup:
+                return lookup[target - number], index
+            else:
+                lookup[number] = index
+
+Solution().twoSum([2, 7, 8], 15)
