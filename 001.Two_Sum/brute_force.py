a3861d6b79f72c030b864cc945f9e2f293ed0e84
victor-lf <victor-lf@hotmail.com>
2018-03-13 00:30:00 -0300

    Update brute_force.py

--- 001.Two_Sum/brute_force.py
+++ 001.Two_Sum/brute_force.py
@@ -1,7 +1,6 @@
 # Brute force approach
 
-class Solution:
-
+class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
